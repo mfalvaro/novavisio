95182178d95385c1b9f90c6d75c55a3c32a47ed3
mfalvaro <mfalvaro@gmail.com>
2024-07-13 17:03:24 -0300

    Tópicos de complex num

--- complexNum/urls.py
+++ complexNum/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home_cNum, name='home_cNum'),
+    path('topicos/', views.topicos_cNum, name='topicos_cNum'),
 ]
 
 
--- complexNum/views.py
+++ complexNum/views.py
@@ -19,3 +19,20 @@ def home_cNum(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'complexNum/home_cNum.html', context=context)
+
+
+
+def topicos_cNum(request):
+    """View function for home page of site."""
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = 0
+
+
+
+    context = {
+        'num_visits': num_visits,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'complexNum/Estrutura_topicos.htm', context=context)
